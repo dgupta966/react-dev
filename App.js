f41f136ae8944be5b169a14413cf83b5b8a9d0d8
Deepak Gupta <dgupta966@gmail.com>
2023-07-18 09:51:06 +0530

    add parcel and react

--- App.js
+++ App.js
@@ -1,1 +1,49 @@
-console.log('dpk')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+    // # parcel 
+    // creating a server
+	// HMR - hot module replacement -> doing this parcel or bundler
+	// File watcher algorithm - written in C++
+	// Bundling
+	// minify
+	// cleaning our code
+	// dev and production build
+	// super fast build algorithm
+	// image optimization
+	// caching while development
+	// compression - e.g., rename variables, etc 
+	// compatible of older version of browser 
+	// it also add polyfill
+	// HTTPS on dev --> npx parcel index.html --https(may be complaint because of localhost and chrome doesn't trust localhost)
+	// manages port numbers
+	// consistent hashing --> that parcel uses for caching
+	// tree shaking -> removing unwanted code
+	
+
+const heading = React.createElement(
+    "h1",
+    {
+        id:"title",
+    },
+    "Heading 1"
+)
+
+const heading2 = React.createElement(
+    "h1",
+    {
+        id:"title",
+    },
+    "Heading 2"
+)
+
+const container = React.createElement(
+    "div",
+    {
+        id:"container"
+    },
+    [heading, heading2]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container)
