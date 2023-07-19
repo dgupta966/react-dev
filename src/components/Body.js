319d67df6c27fa96f8c0b82b6b5be7f41e756a7e
Deepak Gupta <dgupta966@gmail.com>
2023-07-19 11:46:11 +0530

    swiggy api used by localhost via chrome extenstion - allow CORS

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,6 +13,9 @@ const Body = () => {
   }, []);
 
   const getData = async () => {
+    // we can use this api in localhost via chrome extension -> Allow CORS: Access-Control-Allow-Origin
+    // https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf/related
+
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&page_type=DESKTOP_WEB_LISTING"
     );
