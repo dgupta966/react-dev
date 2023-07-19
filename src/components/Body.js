7de4d0113681cdd61b5e870b6e9e6547106b95d4
Deepak Gupta <dgupta966@gmail.com>
2023-07-19 08:28:18 +0530

    implement search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,43 @@
 import RestaurantsCard from "./RestaurantsCard";
 import { restaurantData } from "./constants";
+import { useState } from "react";
+
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantData);
+  const [searchInput, setSearchInput] = useState("");
+
+  const filterData = (searchInput, restaurants) => {
+    const filterData = restaurants.filter((restaurant) =>
+      restaurant.data.name.includes(searchInput)
+    );
+    return filterData;
+  };
+
   return (
-    <div className="restaurant-list">
-      {restaurantData?.map((item) => (
-        <RestaurantsCard key={item.data.id} {...item.data} />
-      ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            const data = filterData(searchInput, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants?.map((item) => (
+          <RestaurantsCard key={item.data.id} {...item.data} />
+        ))}
+      </div>
+    </>
   );
 };
 
