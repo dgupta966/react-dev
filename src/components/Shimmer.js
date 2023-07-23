2763d60467778c0c74afdbfc63efcf8a1188d054
Deepak Gupta <dgupta966@gmail.com>
2023-07-23 11:33:37 +0530

    update shimmer with tailwind

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,11 +1,11 @@
 const Shimmer = () => {
   return (
     <div>
-      <div className="restaurant-list">
-        {Array(12)
+      <div className="flex flex-wrap m-2 p-2">
+        {Array(20)
           .fill("")
           .map((item, index) => (
-            <div key={index} className="shimmer-card"></div>
+            <div key={index} className="h-52 w-52 m-2 p-2 bg-gray-300 "></div>
           ))}
       </div>
     </div>
